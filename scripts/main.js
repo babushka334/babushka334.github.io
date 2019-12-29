6c0e922618a716269fd607a2cd0fda3cfc27ece1
babushka334 <kalini.kostyabtc@gmail.com>
2019-12-29 17:50:20 +0300

    adding my files to my repository

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() 
+{
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/skate-ollie.jpg') 
+    {
+      myImage.setAttribute ('src','images/just-ollie.jpg');
+    } 
+    else 
+    {
+      myImage.setAttribute ('src','images/skate-ollie.jpg');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Привет, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Рады видеть тебя сново, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
